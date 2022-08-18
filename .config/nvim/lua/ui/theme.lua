6b5f7690a9dbf2151c3451a227a0ae25b0233728
Vlad Trishch <vlad.trishch@gmail.com>
2022-08-18 22:14:37 +0300

    Fix telescope background

--- .config/nvim/lua/ui/theme.lua
+++ .config/nvim/lua/ui/theme.lua
@@ -10,5 +10,6 @@ hl(0, "Visual", { bg = c.dark_gray })
 hl(0, "VertSplit", { fg = c.dark_gray, bg = c.alt_bg })
 
 hl(0, "NvimTreeNormal", { bg = c.bg })
+hl(0, "TelescopeNormal", { bg = c.bg })
 hl(0, "NvimTreeGitNew", { fg = c.fg })
 hl(0, "lualine_c_inactive", { fg = c.context, bg = c.bg })
