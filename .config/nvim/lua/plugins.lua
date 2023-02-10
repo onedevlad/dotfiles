106011bc43f2d47038e62781234ac7aa7deee01c
Vlad Trishch <vlad.trishch@gmail.com>
2023-02-10 10:15:58 +0200

    Migrate to Lazy.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,83 +1,55 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
-
--- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
+require("lazy").setup({
+  "wbthomason/packer.nvim",
+  "kyazdani42/nvim-web-devicons",
+  "kyazdani42/nvim-tree.lua",
+  "nvim-lualine/lualine.nvim",
+  "lunarvim/darkplus.nvim",
+  "joeytwiddle/sexy_scroller.vim",
+  "easymotion/vim-easymotion",
+  "vijaymarupudi/nvim-fzf",
+  "akinsho/bufferline.nvim",
+  "moll/vim-bbye",
+  "tpope/vim-fugitive",
+  "lewis6991/gitsigns.nvim",
+  "nvim-lua/plenary.nvim",
+  { "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" },
+  {
+    "nvim-telescope/telescope.nvim",
+    requires = { {"nvim-lua/plenary.nvim"} }
   },
-}
+  { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+  "bkad/CamelCaseMotion",
+  "norcalli/nvim-colorizer.lua",
+  -- "lukas-reineke/indent-blankline.nvim",
 
-return packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'kyazdani42/nvim-web-devicons'
-  use 'kyazdani42/nvim-tree.lua'
-  use 'nvim-lualine/lualine.nvim'
-  use 'lunarvim/darkplus.nvim'
-  use 'joeytwiddle/sexy_scroller.vim'
-  use 'easymotion/vim-easymotion'
-  use 'vijaymarupudi/nvim-fzf'
-  use "akinsho/bufferline.nvim"
-  use "moll/vim-bbye"
-  use "tpope/vim-fugitive"
-  use 'lewis6991/gitsigns.nvim'
-  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use 'bkad/CamelCaseMotion'
-  use 'norcalli/nvim-colorizer.lua'
-  -- use "lukas-reineke/indent-blankline.nvim"
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-buffer",
+  "L3MON4D3/LuaSnip",
+  "hrsh7th/cmp-nvim-lsp",
+  "jose-elias-alvarez/null-ls.nvim",
+  "neovim/nvim-lspconfig",
+  "williamboman/nvim-lsp-installer",
+  "numToStr/Comment.nvim",
+  "kevinhwang91/nvim-bqf",
 
-  use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-path"
-  use "hrsh7th/cmp-buffer"
-  use "L3MON4D3/LuaSnip"
-  use "hrsh7th/cmp-nvim-lsp"
-  use "jose-elias-alvarez/null-ls.nvim"
-
-  use "neovim/nvim-lspconfig"
-  use "williamboman/nvim-lsp-installer"
-  use "numToStr/Comment.nvim"
-  use "kevinhwang91/nvim-bqf"
-
-  -- Treesitter
-  use {
+  {
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
-  }
-  use 'JoosepAlviste/nvim-ts-context-commentstring'
-  use 'nvim-treesitter/playground'
-end)
+  },
+  "JoosepAlviste/nvim-ts-context-commentstring",
+  "nvim-treesitter/playground"
+})
