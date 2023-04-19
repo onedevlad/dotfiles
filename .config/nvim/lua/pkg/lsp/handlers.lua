a55f8f7cd381d9eb7a7b4476f3444672ed2903f8
Vlad Trishch <vlad.trishch@gmail.com>
2023-04-19 22:24:32 +0300

    Palette updates

--- .config/nvim/lua/pkg/lsp/handlers.lua
+++ .config/nvim/lua/pkg/lsp/handlers.lua
@@ -83,7 +83,7 @@ local function lsp_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gj", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gk", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format { async = true }' ]]
 end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
