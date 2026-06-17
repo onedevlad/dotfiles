ddb3bdb34706ed86c65c3768aa0863e8f77ccbc6
Vlad Trishch <vlad.trishch@gmail.com>
2026-06-17 17:49:04 +0300

    Fix pasting text from clipboard

--- .wezterm.lua
+++ .wezterm.lua
@@ -83,6 +83,8 @@ config.enable_csi_u_key_encoding = true
 config.leader = { key = 'a', mods = 'ALT', timeout_milliseconds = 1000 }
 
 config.keys = {
+  { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+
   -- Pane navigation (no prefix): M-C-hjkl
   { key = 'h', mods = 'ALT|CTRL', action = act.ActivatePaneDirection 'Left' },
   { key = 'j', mods = 'ALT|CTRL', action = act.ActivatePaneDirection 'Down' },
